55db8054163e0f05fa4f56cdaa8a538943ecc0fe
Christopher Mlalazi <chrismlalazi@gmail.com>
2016-09-05 15:21:35 -0500

    jQuery

--- js/main.js
+++ js/main.js
@@ -8,8 +8,8 @@ $('.hamburger').on('click', function(e) {
 
 $(document).ready(function () {
   $('button').click(function () {
-    $(this).addClass("selected");
     $("button").removeClass("selected");
+       $(this).addClass("selected");
 		 var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
 		var animal = $(this).text();
 		var flickrOptions = {
