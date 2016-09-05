4c177b5124cbd672cdc874cc713d60d3bda59039
Christopher Mlalazi <chrismlalazi@gmail.com>
2016-09-05 18:17:07 -0500

    Call

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
        success: function(data) {
        	console.log('success', data)
        	$.each(data, function(i, data){
-          $photos.append('<li>' + data + '</li>');
+          $photos.append('<li>'+ $photos + '</li>');
        	});
        }
 
