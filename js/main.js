94301ae7b82641c955c5c3b3bdab56ab2e8c1a78
Christopher Mlalazi <chrismlalazi@gmail.com>
2016-09-05 15:12:35 -0500

    Flicker

--- js/main.js
+++ js/main.js
@@ -6,6 +6,36 @@ $('.hamburger').on('click', function(e) {
 });
 
 
+$(document).ready(function () {
+  $('button').click(function () {
+    $(this).addClass("selected");
+    $("button").removeClass("selected");
+		 var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+		var animal = $(this).text();
+		var flickrOptions = {
+		tags: animal,
+			format: "json"
+		};
+		function displayPhotos(data){
+			var photoHTML = '<ul>';
+			$.each(data.items, function(i, photo){
+				photoHTML += '<li class+"grid-25 table-grid-50">';
+				photoHTML += '<a href="' + photo.link + '" class="image">';
+				photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+				photoHTML += '</ul>';
+				$('#photos').html(photoHTML);
+			});
+			
+		}
+		$.getJSON(flickerAPI, flickrOptions, displayPhotos);
+		
+       
+  });
+}); // end ready
+
+
+
+
 
 
 
