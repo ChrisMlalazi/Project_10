afc662c792b438b89db786d8b396ac4f2e4b6c76
Christopher Mlalazi <chrismlalazi@gmail.com>
2016-09-06 16:57:35 -0500

    New

--- js/main.js
+++ js/main.js
@@ -1,34 +1,26 @@
-$('.hamburger').on('click', function(e) {
-  // Prevent link from jumping to the top of the page
-  e.preventDefault();
-  // If menu is already showing, slide it up. Otherwise, slide it down.
-  $('.menu').toggleClass('slide-down');
-});
-
-
 $(document).ready(function () {
-  var $photos = $('#photos');
-  $("button").click(function(){
-  $.ajax({
-       type: 'GET',
-       url: 'https://pokeapi.co/api/v2/',
-       success: function(data) {
-       	console.log('success', data)
-       	$.each(data, function(i, data){
-          $photos.html(data);
-       	});
-       }
-
-  }); 
-  });     
- 
+  $('button').click(function () {
+    $("button").removeClass("selected");
+     $(this).addClass("selected");
+     var flickerAPI = "http://pokeapi.co/api/v2/ability/4/?jsoncallback=?";
+    var animal = $(this).text();
+    var flickrOptions = {
+    tags: animal,
+      format: "json"
+    };
+    function displayPhotos(data){
+      var photoHTML = '<ul>';
+      $.each(data.items, function(i, photo){
+        photoHTML += '<li class = "grid-25 table-grid-50">';
+        photoHTML += '<a href="' + photo.link + '" class="image">';
+        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+        photoHTML += '</ul>';
+        $('#photos').html(photoHTML);
+      });
+      
+    }
+    $.getJSON(flickerAPI, flickrOptions, displayPhotos);
+    
+       
+  });
 }); // end ready
-
-
-
-
-
-
-
-
-
