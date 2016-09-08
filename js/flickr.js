f58f780ddcace098f88b81b0f074577c1e613ea8
Christopher Mlalazi <chrismlalazi@gmail.com>
2016-09-08 15:27:49 -0500

    CCC

--- js/flickr.js
+++ js/flickr.js
@@ -10,22 +10,25 @@ $(document).ready(function() {
     // the AJAX part
     var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
     var cities = $(this).text();
-    var flickrOptions = {
+    var choice = {
       tags: cities,
       format: "json"
     };
     function displayPhotos(data) {
-      var photoHTML = '<ul>';
+      var photoHTML = '<ul class="flick">';
       $.each(data.items,function(i,photo) {
-        photoHTML += '<li class="grid-06 tablet-grid-06">';
+        photoHTML += '<li class="grid-25>';
         photoHTML += '<a href="' + photo.link + '" class="image">';
         photoHTML += '<img src="' + photo.media.m + '"></a></li>';
       }); // end each
       photoHTML += '</ul>';
       $('#photos').html(photoHTML);
-    }
-    $.getJSON(flickerAPI, flickrOptions, displayPhotos);
+       
+    };
+      
+
+
+    $.getJSON(flickerAPI, choice, displayPhotos);
 
   }); // end click
-
 }); // end ready
